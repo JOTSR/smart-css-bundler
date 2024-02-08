a827894889cb969bcbca904dd78f21f7a262cb92
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-02-08 11:10:18 +0100

    fix: use original url for media_types import

--- deps.ts
+++ deps.ts
@@ -6,9 +6,9 @@ export {
 	toFileUrl,
 } from 'https://deno.land/std@0.181.0/path/mod.ts'
 export { ensureDir } from 'https://deno.land/std@0.192.0/fs/ensure_dir.ts'
+export { contentType } from 'https://deno.land/std@0.213.0/media_types/mod.ts'
 export {
 	type MiddlewareHandler,
 	type Plugin,
 } from 'https://deno.land/x/fresh@1.6.3/server.ts'
-export { contentType } from 'https://deno.land/x/fresh@1.6.3/src/server/deps.ts'
 export { bundleAsync } from 'npm:lightningcss@1.22.1'
