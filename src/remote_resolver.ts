8b361a247b0b3b04ec34a32121276cbd03157367
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-06-10 17:44:17 +0200

    refactor: :recycle: split remote resolver plugin in functions

--- src/remote_resolver.ts
+++ src/remote_resolver.ts
@@ -1,4 +1,4 @@
-import { type PluginBuild } from 'esbuild'
+import { OnResolveArgs, OnResolveResult, type PluginBuild } from 'esbuild'
 import { cachePathToRemoteUrl } from '../utils.ts'
 import { BundleOptions } from './bundle.ts'
 import { cacheAssetFile, cacheRemoteFile } from './caching.ts'
@@ -17,82 +17,22 @@ type RemoteResolverOptions = Omit<
  * @returns Esbuild remote resolver plugin
  */
 export function remoteResolver(
-	{ cacheDir, assetDir, assetNaming, dev, logger }: RemoteResolverOptions,
+	options: RemoteResolverOptions,
 ) {
+	const { dev, logger } = options
+
 	return {
 		name: 'css-remote-resolver',
 		setup(build: PluginBuild) {
-			build.onResolve({ filter: /.*/ }, async (args) => {
+			build.onResolve({ filter: /.*/ }, (args) => {
 				// Resolve absolute remote css and assets
 				if (/^(https?:\/\/).*/.test(args.path)) {
-					logger?.info('resolving absolute remote file', args.path)
-
-					// Get remote file
-					const response = await fetch(args.path)
-					const loader = getLoader(args.path, response)
-
-					// Resolve remote css
-					if (loader === 'css') {
-						const cacheFile = await cacheRemoteFile(cacheDir, response)
-						return {
-							path: cacheFile,
-							namespace: 'remote',
-							pluginData: { loader },
-						}
-					}
-
-					// Resolve remote asset
-					const assetFilePath = await cacheAssetFile(assetDir, response)
-					const assetFile = await assetNaming?.(assetFilePath) ?? assetFilePath
-					return {
-						path: assetFile,
-						namespace: 'remote',
-						pluginData: { loader },
-						external: true,
-					}
+					return resolveAbsoluteRemote(options, args)
 				}
 
 				// Resolve relative remote assets
 				if (args.namespace === 'remote') {
-					// Don't resolve data url
-					if (args.path.startsWith('data:')) {
-						return {
-							path: args.path,
-							external: true,
-							namespace: 'remote',
-							pluginData: { loader: 'dataurl' },
-						}
-					}
-
-					// Resolve relative asset
-					if (args.kind === 'url-token') {
-						// Get absolute url
-						const importerUrl = cachePathToRemoteUrl(cacheDir, args.importer)
-						const url = new URL(args.path, importerUrl)
-
-						logger?.info(
-							`resolving relative remote asset from (${importerUrl.href})`,
-							args.path,
-						)
-
-						// Resolve remote asset
-						const response = await fetch(url)
-						const assetFilePath = await cacheAssetFile(assetDir, response)
-						const loader = getLoader(url.pathname, response)
-						const assetFile = await assetNaming?.(assetFilePath) ??
-							assetFilePath
-
-						return {
-							path: assetFile,
-							external: true,
-							namespace: 'remote',
-							pluginData: { loader },
-						}
-					}
-
-					throw new Error(
-						`unsupported source path kind for css bundle (kind: ${args.kind})`,
-					)
+					return resolveRelativeRemote(options, args)
 				}
 
 				// Watching local files
@@ -113,4 +53,94 @@ export function remoteResolver(
 			})
 		},
 	}
+}
+
+/**
+ * Resolves remote css and assets from their absolute url
+ *
+ * @param options Resolver options
+ * @param args Esbuild context
+ * @returns Esbuild resolve result
+ */
+async function resolveAbsoluteRemote(
+	{ cacheDir, assetDir, assetNaming, logger }: RemoteResolverOptions,
+	{ path }: OnResolveArgs,
+): Promise<OnResolveResult> {
+	logger?.info('resolving absolute remote file', path)
+
+	// Get remote file
+	const response = await fetch(path)
+	const loader = getLoader(path, response)
+
+	// Resolve remote css
+	if (loader === 'css') {
+		const cacheFile = await cacheRemoteFile(cacheDir, response)
+		return {
+			path: cacheFile,
+			namespace: 'remote',
+			pluginData: { loader },
+		}
+	}
+
+	// Resolve remote asset
+	const assetFilePath = await cacheAssetFile(assetDir, response)
+	const assetFile = await assetNaming?.(assetFilePath) ?? assetFilePath
+	return {
+		path: assetFile,
+		namespace: 'remote',
+		pluginData: { loader },
+		external: true,
+	}
+}
+
+/**
+ * Resolves remote css and assets from their relative url
+ *
+ * @param options Resolver options
+ * @param args Esbuild context
+ * @returns Esbuild resolve result
+ */
+async function resolveRelativeRemote(
+	{ cacheDir, assetDir, assetNaming, logger }: RemoteResolverOptions,
+	args: OnResolveArgs,
+): Promise<OnResolveResult> {
+	// Don't resolve data url
+	if (args.path.startsWith('data:')) {
+		return {
+			path: args.path,
+			external: true,
+			namespace: 'remote',
+			pluginData: { loader: 'dataurl' },
+		}
+	}
+
+	// Resolve relative asset
+	if (args.kind === 'url-token') {
+		// Get absolute url
+		const importerUrl = cachePathToRemoteUrl(cacheDir, args.importer)
+		const url = new URL(args.path, importerUrl)
+
+		logger?.info(
+			`resolving relative remote asset from (${importerUrl.href})`,
+			args.path,
+		)
+
+		// Resolve remote asset
+		const response = await fetch(url)
+		const assetFilePath = await cacheAssetFile(assetDir, response)
+		const loader = getLoader(url.pathname, response)
+		const assetFile = await assetNaming?.(assetFilePath) ??
+			assetFilePath
+
+		return {
+			path: assetFile,
+			external: true,
+			namespace: 'remote',
+			pluginData: { loader },
+		}
+	}
+
+	throw new Error(
+		`unsupported source path kind for css bundle (kind: ${args.kind})`,
+	)
 }
