f814f8ca8c84570f897b80f1ee9f44a49c85ed17
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-06-10 17:32:36 +0200

    feat: :sparkles: add local file watcher

--- src/remote_resolver.ts
+++ src/remote_resolver.ts
@@ -22,75 +22,87 @@ export function remoteResolver(
 	return {
 		name: 'css-remote-resolver',
 		setup(build: PluginBuild) {
-			// Resolve absolute remote css and assets
-			build.onResolve({ filter: /^https?:\/\// }, async (args) => {
-				logger?.info('resolving absolute remote file', args.path)
+			build.onResolve({ filter: /.*/ }, async (args) => {
+				// Resolve absolute remote css and assets
+				if (/^(https?:\/\/).*/.test(args.path)) {
+					logger?.info('resolving absolute remote file', args.path)
 
-				// Get remote file
-				const response = await fetch(args.path)
-				const loader = getLoader(args.path, response)
+					// Get remote file
+					const response = await fetch(args.path)
+					const loader = getLoader(args.path, response)
 
-				// Resolve remote css
-				if (loader === 'css') {
-					const cacheFile = await cacheRemoteFile(cacheDir, response)
-					return {
-						path: cacheFile,
-						namespace: 'remote',
-						pluginData: { loader },
+					// Resolve remote css
+					if (loader === 'css') {
+						const cacheFile = await cacheRemoteFile(cacheDir, response)
+						return {
+							path: cacheFile,
+							namespace: 'remote',
+							pluginData: { loader },
+						}
 					}
-				}
-
-				// Resolve remote asset
-				const assetFilePath = await cacheAssetFile(assetDir, response)
-				const assetFile = await assetNaming?.(assetFilePath) ?? assetFilePath
-				return {
-					path: assetFile,
-					namespace: 'remote',
-					pluginData: { loader },
-					external: true,
-				}
-			})
-
-			// Resolve relative remote assets
-			build.onResolve({ filter: /.*/, namespace: 'remote' }, async (args) => {
-				// Don't resolve data url
-				if (args.path.startsWith('data:')) {
-					return {
-						path: args.path,
-						external: true,
-						namespace: 'remote',
-						pluginData: { loader: 'dataurl' },
-					}
-				}
-
-				// Resolve relative asset
-				if (args.kind === 'url-token') {
-					// Get absolute url
-					const importerUrl = cachePathToRemoteUrl(cacheDir, args.importer)
-					const url = new URL(args.path, importerUrl)
-
-					logger?.info(
-						`resolving relative remote asset from (${importerUrl.href})`,
-						args.path,
-					)
 
 					// Resolve remote asset
-					const response = await fetch(url)
 					const assetFilePath = await cacheAssetFile(assetDir, response)
-					const loader = getLoader(url.pathname, response)
 					const assetFile = await assetNaming?.(assetFilePath) ?? assetFilePath
-
 					return {
 						path: assetFile,
-						external: true,
 						namespace: 'remote',
 						pluginData: { loader },
+						external: true,
 					}
 				}
 
-				throw new Error(
-					`unsupported source path kind for css bundle (kind: ${args.kind})`,
-				)
+				// Resolve relative remote assets
+				if (args.namespace === 'remote') {
+					// Don't resolve data url
+					if (args.path.startsWith('data:')) {
+						return {
+							path: args.path,
+							external: true,
+							namespace: 'remote',
+							pluginData: { loader: 'dataurl' },
+						}
+					}
+
+					// Resolve relative asset
+					if (args.kind === 'url-token') {
+						// Get absolute url
+						const importerUrl = cachePathToRemoteUrl(cacheDir, args.importer)
+						const url = new URL(args.path, importerUrl)
+
+						logger?.info(
+							`resolving relative remote asset from (${importerUrl.href})`,
+							args.path,
+						)
+
+						// Resolve remote asset
+						const response = await fetch(url)
+						const assetFilePath = await cacheAssetFile(assetDir, response)
+						const loader = getLoader(url.pathname, response)
+						const assetFile = await assetNaming?.(assetFilePath) ??
+							assetFilePath
+
+						return {
+							path: assetFile,
+							external: true,
+							namespace: 'remote',
+							pluginData: { loader },
+						}
+					}
+
+					throw new Error(
+						`unsupported source path kind for css bundle (kind: ${args.kind})`,
+					)
+				}
+
+				// Watching local files
+				if (dev) {
+					logger?.info(
+						'watching local source file',
+						args.path,
+					)
+					return { watchFiles: [args.path] }
+				}
 			})
 
 			// Load cache files (css)
