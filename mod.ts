8c5fcf623a8985b9271ee7b99525a1a65ccc8c15
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-06-11 11:45:53 +0200

    docs: :memo: update module docs

--- mod.ts
+++ mod.ts
@@ -1,5 +1,9 @@
 /**
- * Remote css bundler.
+ * # Smart css bundler
+ *
+ * Bundle local and remote css files into a single bundle.
+ *
+ * Cache remote css locally and save remote assets to a specified static directory.
  * @module
  */
 export { bundle, type BundleOptions } from './src/bundle.ts'
--- plugins/fresh/mod.ts
+++ plugins/fresh/mod.ts
@@ -1,5 +1,13 @@
 /**
- * Smart css bundle plugin for [deno/fresh](https://fresh.deno.dev).
+ * # Smart css bundle plugin for [deno/fresh](https://fresh.deno.dev)
+ *
+ * Bundle local and remote css files into a single bundle.
+ *
+ * Cache remote css locally and save remote assets to a specified static directory.
+ *
+ * ## Fresh plugin
+ *
+ * Fresh plugin that local and remote bundle css to a single minified local file.
  *
  * @module
  */
